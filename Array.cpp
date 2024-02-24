636752285990a231db242d31a9a595d1a99c73fe
Dinh Van Thang <dinhvanthang0908@gmail.com>
2024-02-24 21:00:02 +0700

    2. Median of two sorted array

--- Array.cpp
+++ Array.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 
 using namespace std;
 
@@ -16,8 +17,9 @@ std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec) {
     return os;
 }
 
-//Two Sum
+//1. Two Sum
 vector<int> TwoSum(vector<int> &numbers, int target){
+    //solution 1
     // int size = numbers.size();
     // for(int i=0; i<size-1; i++){
     //     for(int j=i+1; j<size; j++){
@@ -25,6 +27,8 @@ vector<int> TwoSum(vector<int> &numbers, int target){
     //     }
     // }
     // return {};
+
+    //solution 2
     int size = numbers.size();
     sort(numbers.begin(), numbers.end());
     int left = 0;
@@ -37,15 +41,45 @@ vector<int> TwoSum(vector<int> &numbers, int target){
         else return {left, right};
     }
     return {};
+
+    //Solution 3
+    // int size = numbers.size();
+    // unordered_map<int, int> map_number;
+    // for(int i=0;i<size;i++){
+    //     int remain = target - numbers[i];
+    //     if(map_number.count(remain) != 0){
+    //         return {i, map_number[remain]};
+    //     }
+    //     map_number[numbers[i]] = i;
+    // }
+    // return {};
 }
 
+//2. Median of 2 sorted array
+double MedianValue(vector<int> &nums1, vector<int> &nums2){
+    vector<int> sortedArray;
+    for(auto num:nums1){
+        sortedArray.push_back(num);
+    }
+    for(auto num:nums2){
+        sortedArray.push_back(num);
+    }
+    sort(sortedArray.begin(), sortedArray.end());
+    int size = sortedArray.size();
+    if(size%2==1){
+        return sortedArray[size/2];
+    }
+    else{
+        double temp = (sortedArray[size/2] + sortedArray[size/2-1])/2;
+        return temp;
+    }
+}
 int main(){
-    vector<int> nums = {2,7,11,15,16};
-    vector<int> result;
-    int target = 31;
-    result = TwoSum(nums, target);
+    vector<int> nums1 = {1,2};
+    vector<int> nums2 = {3,4};
+    double result;
+    result = MedianValue(nums1, nums2);
     cout<<result;
-
+    
     return 0;
-
 }
